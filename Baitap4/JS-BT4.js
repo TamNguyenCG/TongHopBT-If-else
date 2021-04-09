be776036d99973ceb07db231fe3b047f13a888d2
TamNguyenCG <ngductam91@gmail.com>
2021-04-09 16:47:53 +0700

    If-else1-Bai4

---
+++ Baitap4/JS-BT4.js
@@ -0,0 +1,11 @@
+let a=+prompt('Nhập số a:')
+let b=+prompt('Nhập số b:')
+let c=+prompt('Nhập số c:')
+if(a>b&&a>c){
+    alert(a+' là số lớn nhất')
+}else if (b>c){
+    alert(b+' là số lớn nhất')
+}else{
+    alert(c+' là số lớn nhất')
+}
+
